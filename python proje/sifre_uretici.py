59fd4af310f320f825fe9d673fe273c44259af0e
Mert Doğuç <mertdoguc@hotmail.com>
2025-01-09 22:26:33 +0300

    sifre_uretici.py

--- python proje/sifre_uretici.py
+++ python proje/sifre_uretici.py
@@ -3,7 +3,7 @@ import random
 def sifre(uzunluk):
     kullanici_sifre = ""
     for i in range(uzunluk):
-        sifre_karakterleri = chr(random.randint(32, 126))
+        sifre_karakterleri = chr(random.randint(33, 126))
         kullanici_sifre += sifre_karakterleri
     return kullanici_sifre
 
