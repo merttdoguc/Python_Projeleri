a54c1982afad99ec921b5af78429aa5bab1d0e02
Mert Doğuç <mertdoguc@hotmail.com>
2025-02-07 14:02:18 +0300

    not_ortalama.py

--- python proje/not_ortalama.py
+++ python proje/not_ortalama.py
@@ -10,29 +10,33 @@ while True:
         final = int(input(f"{a} adlı öğrencinin final notunu giriniz: "))
         ortalama = vize * 0.4 + final * 0.6
         
-        if ortalama >= 95:
-            harf_notu = "A1"
-        elif ortalama >= 90:
-            harf_notu = "A2"
-        elif ortalama >= 85:
-            harf_notu = "A3"
-        elif ortalama >= 80:
-            harf_notu = "B1"
-        elif ortalama >= 75:
-            harf_notu = "B2"
-        elif ortalama >= 70:
-            harf_notu = "B3"
-        elif ortalama >= 65:
-            harf_notu = "C1"
-        elif ortalama >= 60:
-            harf_notu = "C2"
-        elif ortalama >= 55:
-            harf_notu = "C3"
-        elif 45 <= ortalama < 55:
-            harf_notu = "D"
+        if final < 55:
+            harf_notu = "F"
+            durum = "Kaldı"
         else:
-            harf_notu = "F"   
-            
+            if ortalama >= 95:
+                harf_notu = "A1"
+            elif ortalama >= 90:
+                harf_notu = "A2"
+            elif ortalama >= 85:
+                harf_notu = "A3"
+            elif ortalama >= 80:
+                harf_notu = "B1"
+            elif ortalama >= 75:
+                harf_notu = "B2"
+            elif ortalama >= 70:
+                harf_notu = "B3"
+            elif ortalama >= 65:
+                harf_notu = "C1"
+            elif ortalama >= 60:
+                harf_notu = "C2"
+            elif ortalama >= 55:
+                harf_notu = "C3"
+            elif 45 <= ortalama < 55:
+                harf_notu = "D"
+            else:
+                harf_notu = "F"   
+
         if harf_notu in ["C3", "C2", "C1", "B3", "B2", "B1", "A3", "A2", "A1"]:
             durum = "Geçti"
         elif harf_notu == "D":
